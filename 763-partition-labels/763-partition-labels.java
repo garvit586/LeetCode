7e593d22a1251dead671537df95e11dfc43abab2
garvit586 <60777423+garvit586@users.noreply.github.com>
2022-05-18 15:06:47 +0530

    Time: 4 ms (82.73%), Space: 40.4 MB (98.23%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -2,12 +2,12 @@ class Solution {
     public List partitionLabels(String s) {
         List l=new ArrayList();
         if(s.length()==0) return l;
-        int last[]=new int[26];// to store last occurence of every character
+        int last[]=new int[26];
         for(int i=0;i<s.length();i++)
         {
             last[s.charAt(i)-'a']=i;
         }
-        int end=0,start=0;// to store end and start of each partition
+        int end=0,start=0;
         for(int i=0;i<s.length();i++)
         {
             end=Math.max(end,last[s.charAt(i)-'a']);
